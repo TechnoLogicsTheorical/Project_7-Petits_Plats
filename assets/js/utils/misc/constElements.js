f5b80c46d39bc6b112f44016ef3ea75bbbaef38c
Anthony.M <marcellin.anthony77@gmail.com>
2022-11-27 00:19:15 +0100

    Mise en place de la documentation des constantes

--- assets/js/utils/misc/constElements.js
+++ assets/js/utils/misc/constElements.js
@@ -1,29 +1,51 @@
+/**
+ * Regroupe toutes les constantes des éléments constituant la page au sein d'un même objet afin de facilité l'accès
+ * @property {Object} Containers Contient toutes les constantes correspondant aux conteneurs d'éléments sous-jacents
+ * @property {HTMLDivElement} Containers.recipes Element contenant toutes les cartes de recettes affichées
+ * @property {HTMLDivElement} Containers.actives_filters Element contenant toutes les étiquettes de filtres
+
+
+ * @property {Object} Forms Contient toutes les constantes correspondant aux formulaires de saisies
+ * @property {HTMLCollection} Forms.all Correspondant à un tableau d'éléments HTML contenant tous les formulaires
+ * @property {HTMLFormElement} Forms.recipe Formulaire de saisie ayant le champ permettant de récupérer la valeur de recherche principale
+
+
+ * @property {Object} Inputs Contient toutes les constantes correspondant aux champs de saisie
+ * @property {HTMLInputElement} Inputs.mainBar Element de champ de saisie de texte, utilisée pour récupérer la requête de recherche
+ * @property {HTMLInputElement} Inputs.ingredient Element de champ de saisie pour rechercher un ingrédient
+ * @property {HTMLInputElement} Inputs.equipment Element de champ de saisie pour rechercher un équipement
+ * @property {HTMLInputElement} Inputs.ustensil Element de champ de saisie pour rechercher un ustensile
+
+
+ * @property {Object} DropdownLists Contient toutes les constantes liées aux listes contenant toutes les options de filtre
+ * @property {HTMLUListElement} DropdownLists.ingredient Element de conteneur de liste ingrédient
+ * @property {HTMLUListElement} DropdownLists.equipment Element de conteneur de liste équipement
+ * @property {HTMLUListElement} DropdownLists.ustensil Element de conteneur de liste ustensile
+
+
+ * @property {Object} Buttons Contient tous les boutons de l'interface
+ * @property {HTMLCollection} Buttons.all_dropdowns Tableau de tous les boutons d'ouverture de menu des filtres
+ */
 export const DOM_ELEMENTS = {
-    // All Containers
     Containers: {
         recipes: document.querySelector('#RECIPES'),
         actives_filters: document.querySelector('#ACTIVE_FILTERS'),
     },
-
     Forms: {
         all: document.querySelectorAll('form'),
         recipe: document.querySelector('#recipesSearch'),
     },
-
-    // Form Inputs Addictive Search
     Inputs: {
         mainBar: document.querySelector('#recipesSearch'),
         ingredient: document.querySelector('#ingredientsSearch input'),
         equipment: document.querySelector('#equipmentsSearch input'),
         ustensil: document.querySelector('#ustensilsSearch input'),
     },
-
     DropdownLists: {
         ingredient: document.querySelector('#ingredientsList'),
         equipment: document.querySelector('#equipmentsList'),
         ustensil: document.querySelector('#ustensilsList'),
     },
-
     Buttons: {
         all_dropdowns: document.querySelectorAll('.arrow-icon'),
     }
--- assets/js/utils/misc/eventsCallback.js
+++ assets/js/utils/misc/eventsCallback.js
@@ -3,8 +3,28 @@ import { Tag } from "../components/Tag.js";
 import { Results, SearchEngine } from "../managers/SearchEngine.js";
 import { Interface } from "../Interface.js";
 
+/**
+ * Regroupe tous les événements de l'interface et des comportements nécessaires au bon déroulement des actions
+ * @property {Object} EVENTS Collection de propriété
+ * @property {Object} DropdownLists Collection de fonctions de rappel
+ * @property {function} DropdownLists.clickWithTagItem Fonction de rappel pour l'ajout d'un filtre
+ * @property {function} DropdownLists.toggleListMenu Fonction de rappel pour basculement de l'affichage du conteneur
+ *
+ * @property {Object} Tags Collection de propriété
+ * @property {function} Tags.delete Fonction de rappel pour l'action de suppression d'un filtre
+ *
+ * @property {Object} Forms Collection de propriété
+ * @property {function} Forms.cancelFormAction Fonction de rappel pour annuler le comportement par défaut des formulaires
+ *
+ * @property {Object} Inputs Collection de propriété
+ * @property {function} Inputs.attachEventSearch Fonction de rappel pour récupérer la valeur du champ de saisie
+ */
 export const EVENTS = {
     DropdownLists: {
+        /**
+         * Fonction de rappel pour l'ajout d'un filtre
+         * @param {UIEvent} event Evenement de l'élément
+         */
         clickWithTagItem(event) {
             const element = event.target;
 
@@ -17,6 +37,10 @@ export const EVENTS = {
 
             SearchEngine.proceedSecondSearch();
         },
+        /**
+         * Fonction de rappel pour basculement de l'affichage du conteneur
+         * @param {UIEvent} event Evenement de l'élément
+         */
         toggleListMenu(event) {
             const currentIconButton = event.target;
             const currentListMenu = event.target.parentElement.nextElementSibling;
@@ -32,6 +56,10 @@ export const EVENTS = {
         }
     },
     Tags: {
+        /**
+         * Fonction de rappel pour l'action de suppression d'un filtre
+         * @param {UIEvent} event Evenement de l'élément
+         */
         delete(event) {
             const buttonElement = event.target;
             const element = buttonElement.parentElement.parentElement;
@@ -46,11 +74,19 @@ export const EVENTS = {
         },
     },
     Forms: {
+        /**
+         * Fonction de rappel pour annuler le comportement par défaut des formulaires
+         * @param {UIEvent} event Evenement de l'élément
+         */
         cancelFormAction(event) {
             event.preventDefault();
         }
     },
     Inputs: {
+        /**
+         * Fonction de rappel pour récupérer la valeur du champ de saisie
+         * @param {KeyboardEvent} event Evenement de l'élément
+         */
         attachEventSearch(event) {
             const typedUserValue = event.target.value.toLowerCase();
             if ( typedUserValue.length >= 3 ) {
